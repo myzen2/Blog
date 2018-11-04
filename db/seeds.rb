154bc09bfae1cc7e16ef5aa63321113ccabbe17e
myzen2 <mehdi.zenbil@orange.fr>
2018-11-04 22:38:25 +0100

    style in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create([{title: 'zozo'}, {title: 'nouveau'}])
+#
+#
+100.times do
+  Post.create(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs
+  )
+end
+
+#
+#
+#
+# if Rails.env.development?
+#
+#   users = []
+#   posts = []
+#   #comments = []
+#
+#   # 500.times do |i|
+#   #   users << User.create(username: Faker::Name.name)
+#   # end
+#   #
+#   # 500.times do |i|
+#   #   posts << Post.create(
+#   #       title: Faker::Lorem.sentence,
+#   #       content: Faker::Lorem.paragraphs(5),
+#   #       user_id: users.sample
+#   #   )
+#   # end
+#   #
+#   # # 5000.times do |i|
+#   # #   comments << Comment.create(
+#   # #       content: Faker::Lorem.paragraph,
+#   # #       user: users.sample,
+#   # #       post: posts.sample
+#   # #   )
+#   # # end
+#   #
+#
+p "Created #{Post.count} posts"
+# end
